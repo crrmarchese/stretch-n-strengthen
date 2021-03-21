1ea6ed053380107a2073274bb808509e5311d419
Toni Powell <tonipow3ll@gmail.com>
2021-03-21 17:50:43 -0400

    added some comments

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ const PORT = process.env.PORT || 3001;
 
 // app.use(routes);
 
-// sequelize.sync({ force: false }).then(() => {
-//   app.listen(PORT, () => console.log('Now listening'));
-// });
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
