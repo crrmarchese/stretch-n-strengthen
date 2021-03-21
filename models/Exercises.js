0de6ee60c58af5035c569eb9045606b199b323e3
Toni Powell <tonipow3ll@gmail.com>
2021-03-21 17:50:36 -0400

    models started, added some comments

--- models/Exercises.js
+++ models/Exercises.js
@@ -1,5 +1,18 @@
-class Exercises extends Model {
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const Sequelize = require('../config/connection');
 
-}
+class Exercises extends Model {}
+
+Exercises.init(
+    {
+        id: {
+            type: DataTypes.UUIDV4,
+            defaultValue: Sequelize.UUIDV4,
+            allowNull: false
+        },
+        
+    }
+)
 
 module.exports = { Exercises }
