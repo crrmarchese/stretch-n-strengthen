a4bb8b30c18ad8679c5fb81b0f52897a3f80fe5a
rroyalty <rroyalty@gmail.com>
2021-03-23 10:49:12 -0400

    seeds

--- models/index.js
+++ models/index.js
@@ -3,31 +3,33 @@ const { Category }  = require('./Category')
 const { Muscle } = require('./Muscle');
 const { Equipment } = require('./Equipment');
 const { Exercise_Equipment } = require('./Exercise_Equipment');
-// const { Exercise_Muscle }  = require('./exercise_muscle')
+const { Exercise_Muscle }  = require('./Exercise_Muscle')
 
 
-// Exercise.belongsTo(Category, {
-//     foreignKey: 'category_id',
-//     as: 'Category_ID'
-//   });
+Exercise.belongsTo(Category, {
+    foreignKey: 'category_id',
+    as: 'Category_ID'
+  });
   
-// Category.hasMany(Exercise, {
-//     foreignKey: 'category_id',
-//   });
-// Exercise.belongsToMany(Muscle, {
-//     through: Exercise_Muscle,
-//     foreignKey: 'muscle_id',
-// });
-// Exercise.belongsToMany(Equipment, {
-//     through: Exercise_Equipment,
-//     foreignKey: 'equipment_id',
-// });
-// Muscle.belongsToMany(Exercise, {
-//     through: Exercise_Muscle,
-//     foreignKey: 'exercise_id',
-// });
-// Equipment.belongsToMany(Exercise, {
-//     through: Exercise_Equipment,
-//     foreignKey: 'exercise_id',
-// });
-module.exports = { Exercise, Category, Equipment, Muscle, Exercise_Equipment };
+Category.hasMany(Exercise, {
+    foreignKey: 'category_id',
+  });
+  
+Exercise.belongsToMany(Muscle, {
+    through: Exercise_Muscle,
+    foreignKey: 'muscle_id',
+});
+Exercise.belongsToMany(Equipment, {
+    through: Exercise_Equipment,
+    foreignKey: 'equipment_id',
+});
+Muscle.belongsToMany(Exercise, {
+    through: Exercise_Muscle,
+    foreignKey: 'exercise_id',
+});
+Equipment.belongsToMany(Exercise, {
+    through: Exercise_Equipment,
+    foreignKey: 'exercise_id',
+});
+
+module.exports = { Exercise, Category, Equipment, Muscle, Exercise_Equipment, Exercise_Muscle };
