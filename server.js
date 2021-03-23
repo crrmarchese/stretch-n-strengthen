1ef4f1fc70cc7e5080f1c3b63893a0a79c182cd0
Pythonidaer <pythonidaer@gmail.com>
2021-03-22 21:17:28 -0400

    Created handlebars mockup

--- server.js
+++ server.js
@@ -15,20 +15,31 @@
 
 
 const express = require('express');
-const routes = require('./routes');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const path = require('path');
+// const routes = require('./routes');
 const sequelize = require('./config/connection');
+// const helpers = require('./utils/helpers');
+
+// const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Handlebars stuff, can be found in class assignments
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers/hammond'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(routes);
 
-app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
   });
-})
+});
