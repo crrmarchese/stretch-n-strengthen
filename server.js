c9c2f2ae065b96a1a3d0208a378ddadd7afef462
Toni Powell <tonipow3ll@gmail.com>
2021-03-23 16:26:20 -0400

    heck

--- server.js
+++ server.js
@@ -19,8 +19,12 @@ const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 const path = require('path');
 const passport = require('passport')
+const session = require('express-session')
+
 // const routes = require('./routes');
 const sequelize = require('./config/connection');
+
+require('./config/passport')(passport)
 // const helpers = require('./utils/helpers');
 
 // const hbs = exphbs.create({ helpers });
@@ -31,6 +35,15 @@ const PORT = process.env.PORT || 3001;
 // Handlebars stuff, can be found in class assignments
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+
+// passport middleware
+app.use(passport.initialize);
+
+// feeeeeel like session is not the way to go on this?!
+// app.use(passport.session());
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./controllers/hammond'));
 
