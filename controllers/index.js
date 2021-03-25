4289b8a94d09a1c83a7c456879a4792d597feeb5
Toni Powell <tonipow3ll@gmail.com>
2021-03-24 20:33:52 -0400

    added logout and withauth helper

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
-const authRoutes = require('./auths')
+const authRoutes = require('./auths');
+const withAuth = require('../utils/auth');
 
+// might have to refactor this.. currently set to /api routes require login. 
 router.use('/', homeRoutes);
 router.use('/auth', authRoutes);
-router.use('/api', apiRoutes);
+router.use('/api', withAuth, apiRoutes);
 
 module.exports = router;
