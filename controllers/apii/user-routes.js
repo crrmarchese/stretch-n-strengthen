115345850ceffa2a207288a027078156ff09c991
Pythonidaer <pythonidaer@gmail.com>
2021-03-27 09:25:22 -0400

    Adding handlebars stuff I learned from assignments.

---
+++ controllers/apii/user-routes.js
@@ -0,0 +1,135 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// CREATE new user
+router.post('/', async (req, res) => {
+  try {
+    const dbUserData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    // Set up sessions with a 'loggedIn' variable set to `true`
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Login
+router.post('/login', async (req, res) => {
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (!dbUserData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password. Please try again!' });
+      return;
+    }
+
+    const validPassword = await dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password. Please try again!' });
+      return;
+    }
+
+    // Once the user successfully logs in, set up the sessions variable 'loggedIn'
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res
+        .status(200)
+        .json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Logout
+router.post('/logout', (req, res) => {
+  // When the user logs out, destroy the session
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
+
+
+
+
+// MODELS: User.js
+// const { Model, DataTypes } = require('sequelize');
+// const bcrypt = require('bcrypt');
+// const sequelize = require('../config/connection');
+
+// class User extends Model {
+//   checkPassword(loginPw) {
+//     return bcrypt.compareSync(loginPw, this.password);
+//   }
+}
+
+// User.init(
+//   {
+//     id: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       primaryKey: true,
+//       autoIncrement: true,
+//     },
+//     username: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     email: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//       unique: true,
+//       validate: {
+//         isEmail: true,
+//       },
+//     },
+//     password: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//       validate: {
+//         len: [6],
+//       },
+//     },
+//   },
+//   {
+//     hooks: {
+//       async beforeCreate(newUserData) {
+//         newUserData.password = await bcrypt.hash(newUserData.password, 10);
+//         return newUserData;
+//       },
+//     },
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: 'user',
+//   }
+// );
+
+// module.exports = User;
--- server.js
+++ server.js
@@ -1,29 +1,27 @@
-// const routes = require('./controllers');
-// const helpers = require('./utils/helpers');
-
-// Create the Handlebars.js engine object with custom helper functions
-//const hbs = exphbs.create({ helpers });
+const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-const path = require('path');
-// const routes = require('./routes');
+const routes = require('./controllers/');
+// const helpers = require('./utils/helpers');
+
 const sequelize = require('./config/connection');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+// Create the Handlebars.js engine object with custom helper functions
+// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({});
 
 // Inform Express.js which template engine we're using
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers/hammond'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(routes);
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
