67e3a5333906c667d5925a7688c2555476c0ebf7
Toni Powell <tonipow3ll@gmail.com>
2021-03-28 10:47:52 -0400

    login routes

--- models/user.js
+++ models/user.js
@@ -4,10 +4,11 @@ const sequelize = require('../config/connection')
 const bcrypt = require('bcrypt')
 
 class User extends Model {
-     checkPassword(loginPw) {
-                return bcrypt.compareSync(loginPw, this.password)
+async checkPassword(password) {
+                return await bcrypt.compare(password, this.password)
               }
  }
+ 
 
 User.init(
     {
