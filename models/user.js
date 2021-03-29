7c3067dc1013bcd935e89f332cecfacdc071ed3e
rroyalty <rroyalty@gmail.com>
2021-03-29 18:56:13 -0400

    fg

--- models/user.js
+++ models/user.js
@@ -4,8 +4,8 @@ const sequelize = require('../config/connection')
 const bcrypt = require('bcrypt')
 
 class User extends Model {
-async checkPassword(loginPw) {
-                return await bcrypt.compare(loginPw, this.password)
+     checkPassword(loginPw) {
+                return bcrypt.compareSync(loginPw, this.password)
               }
  }
 
