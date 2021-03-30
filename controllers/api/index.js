2ed5fa9b4e72f6a766c999961b5d6fe67998190e
Toni Powell <tonipow3ll@gmail.com>
2021-03-30 17:18:31 -0400

    fixed

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,10 +3,11 @@ const exerciseRoutes = require('./exercise-routes');
 const muscleRoutes = require('./muscle-routes');
 const equipmentRoutes = require('./equipment-routes');
 const userRoutes = require('./equipment-routes');
-const routineRoutes = require('./routine-routes')
+const routineRoutes = require('./routine-routes');
+// const auths = require('./auths')
 const withAuth = require('../../utils/auth')
 
-
+// router.use('/auth', auths)
 router.use('/exercise', exerciseRoutes);
 router.use('/muscle', muscleRoutes);
 router.use('/equipment', equipmentRoutes);
