3d9c57f179900a33a00a2957410f5aacb0088ec2
Toni Powell <tonipow3ll@gmail.com>
2021-04-01 12:42:40 -0400

    updated google url

--- config/passport.js
+++ config/passport.js
@@ -12,7 +12,7 @@ module.exports = function (passport) {
         passReqToCallback: true,
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: 'http://localhost:3001/auth/google/callback',
+        callbackURL: '/auth/google/callback',
         passReqToCallback   : true
       },
       async (req, accessToken, refreshToken, profile, done) => {
