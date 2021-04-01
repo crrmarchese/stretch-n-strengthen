fd9dd2548af9e80de3e4b4ce1b41b19010da0194
Toni Powell <tonipow3ll@gmail.com>
2021-03-31 20:09:38 -0400

    stuff

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -10,26 +10,18 @@ router.get('/', (req, res) => {
 });
 
 
-// User clicks 'About' nav-item, gets the about route and renders the about template
+
 router.get('/about', (req, res) => {
-  // sample page, equivalent to /muscles -> /muscles/:id -> /exercises -> /exercise/:id
   res.render('about');
-  // we will probably need try/catch auth code on every page to check if user is logged in.
 });
 
 router.get('/muscle', async (req, res) => {
   try {
     const dbMuscleData = await Muscle.findAll({
-      // include: [
-      //   {
-      //     model: Exercise,
-      //   },
-      // ],
     });
     const muscles = dbMuscleData.map((muscle) =>
       muscle.get({ plain: true })
     );
-    // console.log(muscles[0]);
     res.render('muscles', {
       muscles,
     });
@@ -57,8 +49,6 @@ router.get('/muscle/:id', async (req, res) => {
     });
     const muscle = dbMuscleData.get({ plain: true });
     console.log('\n');
-    // console.log(muscle);
-    // console.log(muscle[0]);
     console.log('\n');
     res.render('muscle-specific', { muscle });
   } catch (err) {
@@ -88,12 +78,6 @@ router.get('/exercise/:id', async (req, res) => {
       ],
     });
     const exercise = dbExercise.get({ plain: true });
-    // console.log('test');
-    // console.log(exercise.equipment);
-    // console.log(exercise.equipment[0].name);
-    // console.log(exercise.equipment.name);
-    // console.log(exercise.muscles[0].image_url_main);
-    // console.log(exercise.description);
     res.render('exercise', { exercise });
   } catch (err) {
     console.log(err);
@@ -103,27 +87,17 @@ router.get('/exercise/:id', async (req, res) => {
 
 
 router.get('/routines', (req, res) => {
-
-  // this breaks it 
-  // if (!req.session.loggedIn) { res.redirect('/login'); }
-  // else {
-
-    // we will probably need try/catch auth code on every page to check if user is logged in.    
     res.render('routines');
-  // }
 });
 
 
 // GET ROUTE FOR LOGIN PAGE 
-
-// note this is made with session.loggedIn copy paste
 router.get('/login', async (req, res, next) => {
   res.render('login');
   return;
 });
 
 //  GET ROUTE for signup page
-
 router.get('/signup', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
@@ -133,9 +107,6 @@ router.get('/signup', (req, res) => {
   res.render('signup');
   return;
 });
-
-
-
 
 
 
