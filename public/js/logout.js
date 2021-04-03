97551ecdd0916a5e2d8f79c2a57ece38a98227e8
rroyalty <rroyalty@gmail.com>
2021-04-02 21:20:46 -0400

    jhjhj

--- public/js/logout.js
+++ public/js/logout.js
@@ -3,4 +3,13 @@ const logout = async () => {
     method: `POST`,
     headers: { 'Content-Type': 'application/json' },
   });
+
+if (response.ok) {
+  document.location.replace('/');
+  console.log(response)
+} else {
+  alert('failed to logout')
 }
+};
+
+document.querySelector('#logout').addEventListener('click', logout)
