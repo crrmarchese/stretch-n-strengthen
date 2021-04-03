d97d77842622743ed83ce9da0f1d09295d6d0a1b
rroyalty <rroyalty@gmail.com>
2021-04-02 21:09:27 -0400

    another push

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,18 +1,6 @@
-// const logout = async () => {
-//     const response = await fetch('/api/users/logout', {
-//       method: 'POST',
-//       headers: { 'Content-Type': 'application/json' },
-//     });
-
-    
-  
-//     if (response.ok) {
-//       document.location.replace('/');
-//     } else {
-//       console.log(req.user);
-//       alert('Failed to log out.');
-//     }
-//   };
-  
-//   document.querySelector('#logout').addEventListener('click', logout);
-  
+const logout = async () => {
+  const response = await fetch(`/logout`, {
+    method: `POST`,
+    headers: { 'Content-Type': 'application/json' },
+  });
+}
