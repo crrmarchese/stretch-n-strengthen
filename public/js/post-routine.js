77b8dd45df93c0115044a80b2dfd949e3f2f52a6
rroyalty <rroyalty@gmail.com>
2021-04-03 09:27:14 -0400

    asdfasdf

--- public/js/post-routine.js
+++ public/js/post-routine.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
         event.preventDefault;
         const data = {
                         user_id: $('#profile').data("userid"),
+                        name: "Default Routine Name!",
+                        description: "Here the user will eventually be able to add a detailed description of their routine!"
                     };
         $.post("/api/routine", data).then((response) => { 
             window.location.assign(`/routine/${response.id}`)
